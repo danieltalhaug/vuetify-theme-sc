3dbb2110fe2379718505f570f8fb9e33455e8126
Daniel Talhaug <daniel.talhaug@outlook.com>
2024-06-09 12:36:52 +0200

    Add smartcraft light theme

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,10 +1,16 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { smartcraftLight } from '@/themes';
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ... your configuration
+    theme: {
+        // defaultTheme: 'smartcraftLight',
+        themes: {
+            smartcraftLight,
+        },
+      },
   })
   app.vueApp.use(vuetify)
 })
---
+++ themes/index.ts
@@ -0,0 +1,14 @@
+import type { ThemeDefinition } from 'vuetify';
+
+export const smartcraftLight: ThemeDefinition = {
+    dark: false,
+    colors: {
+      background: '#FFFFFF',
+      surface: '#FFFFFF',
+      primary: '#3e8a8b',
+      secondary: '#817b93',
+      error: '#e1474c',
+      success: '#2a8f5e',
+      warning: '#c06800',
+    },
+}
