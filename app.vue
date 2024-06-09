72f8751fe00d80f5c50a9c008ac604ca362702cf
Daniel Talhaug <daniel.talhaug@outlook.com>
2024-06-09 11:29:49 +0200

    Setup for theming buttons

--- app.vue
+++ app.vue
@@ -1,7 +1,40 @@
+<script setup lang="ts">
+const components = [
+  {
+    title: 'Button',
+    el: 'v-btn',
+    variants: ['elevated', 'flat', 'tonal', 'outlined', 'text', 'plain'],
+  },
+];
+const colors = ['primary', 'success', 'warning', 'error', 'info'];
+</script>
+
 <template>
   <NuxtLayout>
     <v-app>
-      <NuxtWelcome />
+      <v-container>
+        <v-card
+          v-for="component in components"
+          :key="component.el"
+          :title="component.title"
+        >
+          <v-card-text class="d-flex flex-column ga-6">
+            <section
+              v-for="color in colors"
+              :key="color"
+              class="d-flex flex-column flex-md-row justify-space-between align-center ga-4"
+            >
+              <v-btn
+                v-for="variant in component.variants"
+                :key="variant"
+                :color="color"
+                :variant="variant"
+                :text="variant"
+              />
+            </section>
+          </v-card-text>
+        </v-card>
+      </v-container>
     </v-app>
   </NuxtLayout>
 </template>
